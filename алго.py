1e706c37539e88761cc2672dc53c1fdf8d29cf9b
Денис Малич <malichdenis09@mail.ru>
2023-12-26 20:33:44 +0300

    h_001

--- алго.py
+++ алго.py
@@ -1,17 +1,18 @@
 from typing import Union
 
+
 def algo(max_number: str) -> Union[list[int], list[str]]:
-    list: list[int] = []
+    ls: list[int] = []
     try:
         int_number: int = int(max_number)
     except ValueError:
         return ["Нельзя вводить символы отличные от числовых!"]
     for num in range(1, int_number+1):
         up = [num]*num
-        list += up
-    if len(list) == 0:
+        ls += up
+    if len(ls) == 0:
         return ["Число должно быть больше 0!"]
-    return list
+    return ls
 
 
 if __name__ == '__main__':
